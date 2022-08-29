cbb630f707462f5dadd1863074b3994faf6dc754
Ishu Mehta <>
2022-08-29 21:55:10 +0530

    logout working

--- src/COMPONENTS/GoogleAuth.js
+++ src/COMPONENTS/GoogleAuth.js
@@ -1,35 +1,49 @@
+/* eslint-disable no-undef */
 import { React, useEffect, useState } from 'react';
 import jwtDecode from 'jwt-decode';
 
 const GoogleAuth = () => {
-
-    function handle(response) {
-        console.log(jwtDecode(response.credential))
-    }
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
 
-
-
-        // eslint-disable-next-line no-undef
         google.accounts.id.initialize({
             client_id: '340776448169-pv7vta4ad78en7mvf9obtrvnns7e59f6.apps.googleusercontent.com',
-            callback: handle
+            callback: (response) => {
+                setUser(jwtDecode(response.credential))
+            },
         })
 
-        // eslint-disable-next-line no-undef
         google.accounts.id.renderButton(
             document.getElementById("signInDiv"),
             { theme: "outline", size: "large" }
         )
-    });
+
+
+    }, [user]);
+
+    const displayMethod = () => {
+
+        if (user) {
+
+            return (
+                <button id='hey' onClick={() => { setUser(null); }} className='ui secondary button'>Logout</button>
+            )
+        } else {
+            return (
+                <div id='signInDiv'></div>
+            )
+        }
+    }
+
     return (
         <div>
-            <div id='signInDiv'>
-
-            </div>
+            {displayMethod()}
         </div>
+
     );
 }
 
 export default GoogleAuth;
+
+
